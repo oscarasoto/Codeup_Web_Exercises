181f90168ec83030580f4c0c7259c65e0f99b000
Oscar Soto <oscarasotoa@gmail.com>
2016-10-21 12:28:18 -0500

    jQuery exercises.

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -1,11 +1,18 @@
 
-var content = $("#header");
-alert(content.html());
+var $content = $("#header");
+// alert(content.html());
 
-var lists = $(".list");
-alert(lists.html());
+var $lists = $(".list, #header");
+// alert(lists.html());
 
-lists.each(function (element) {
-    console.log($(this).html());
+$lists.each(function (element) {
+    console.log($(this).html()); //current elemtn in the result set
 
 });
+
+$($lists).css("border", "1px solid red");
+
+// set multiple CSS properties
+
+$("#paragraph").css({"background-color":"yellow", "font-size":"15px", "font-weight":"bold"});
+
